c8a6f0f91f821760e29f0a6898eba3b4e4455c28
Joshua Frkuska <j.frkuska@gmail.com>
2013-04-14 17:08:44 +0900

    Initial commit of the LMU library and test application
      The test application is for controlling 2 motors using the L298N DC motor driver
      It is controlled with the help of a serial ModbusSlave library found at https://sites.google.com/site/jpmzometa/arduino-mbrt/arduino-modbus-slave.
    
    Signed-off-by: Joshua Frkuska <j.frkuska@gmail.com>

---
+++ LMUModbusSlaveRTU_ino/LMUModbusSlaveRTU_ino.ino
@@ -0,0 +1,79 @@
+/*
+ Copyright (C) 2013 Joshua Frkuska  DigitalArtisans.Org
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <da_lmu.h>
+#include <ModbusSlave.h>
+
+/* L298N Motor Driver Pins */
+#define M0_EN_PIN 10
+#define M0_P0_PIN 12
+#define M0_P1_PIN 13
+#define M1_EN_PIN 9
+#define M1_P0_PIN 8
+#define M1_P1_PIN 11
+  
+/* create a modbus slave instance */
+ModbusSlave mbs;
+/* create our chassis with 2 motors */
+uchar da_motor::tot_num_mtrs = 0;
+da_dc_brushed motors[] = {
+  da_dc_brushed(CW,255, M0_EN_PIN, M0_P0_PIN, M0_P1_PIN),
+  da_dc_brushed(CW,255, M1_EN_PIN, M1_P0_PIN, M1_P1_PIN)
+};
+da_lmu chassis(motors);
+
+/* modbus slave registers */
+enum {        
+  MB_MTR_TARGET,     /* motor index to apply common settings to */
+  MB_MTR_DIR,        /* BRAKE=0, FORWARD=1, REVERSE=2 */
+  MB_MTR_THROTTLE,   /* 0x00 - 0xFF */
+  MB_REGS	     /* total number of registers on slave */
+};
+
+int regs[MB_REGS];
+
+unsigned long wdog = 0;         /* watchdog */
+unsigned long tprev = 0;        /* previous time*/
+
+void setup() 
+{
+  /* Modbus slave configuration parameters */
+  const unsigned char SLAVE = 1;
+  const long BAUD = 115200;
+  const char PARITY = 'e';
+  const char TXENPIN = 0;
+
+  /* MBS: configure */
+  mbs.configure(SLAVE,BAUD,PARITY,TXENPIN);
+}
+
+void loop()
+{
+  /* main modbus loop*/
+  if(mbs.update(regs, MB_REGS))
+    wdog = millis();
+
+  if ((millis() - wdog) > 5000)  { /* if no comm in 5 sec */
+    /* handle timeout */
+  }        
+
+  /* update motor throttle */
+  if (regs[MB_MTR_TARGET] < da_motor::get_mtr_cnt())
+    motors[regs[MB_MTR_TARGET]].setThrottle(regs[MB_MTR_THROTTLE], (travel)regs[MB_MTR_DIR]);
+}
+
---
+++ libraries/LMU/da_lmu.h
@@ -0,0 +1,90 @@
+/*
+ This library provides the core organizational classes of the LocoMotionUnit 
+ for Arduino based platforms.
+ 
+ Copyright (C) 2013 Joshua Frkuska  DigitalArtisans.Org
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "Arduino.h"
+
+typedef unsigned char uchar;
+
+/* 
+* Name: enum forward_orientation
+* This sets the way the motor turns in order to progress linearly forward */
+typedef enum forward_orientation {
+	CW,
+	CCW
+}orientation;
+
+/* this sets the motor to either move forward or backwards linearly
+ * from its current position */
+typedef enum travel_direction {
+	BRAKE,
+	FORWARD,
+	REVERSE
+}travel;
+
+class da_motor {
+private:
+  orientation forward;			/* cw or ccw */
+  travel direction;			/* present rotation direction */
+  unsigned char throttle_present;       /* present motor throttle */
+  unsigned char throttle_max;		/* max motor throttle count */
+  static uchar tot_num_mtrs;		/* total motors derived from class */
+  unsigned char set_orientation(orientation rotation) { forward = rotation; }
+public:
+  da_motor(orientation fwd, uchar tmax): 
+  forward(fwd), throttle_max(tmax) { tot_num_mtrs++; }
+  virtual int setThrottle(uchar, travel) = 0;
+
+  unsigned char set_throttle(int value) { throttle_present = value & 0xFF; }
+  unsigned char get_throttle(void) { return throttle_present; }
+  orientation get_orientation(void) { return forward; }
+  travel get_direction(void) { return direction; }
+  void set_direction(travel new_dir) { direction = new_dir; }
+  static unsigned char get_mtr_cnt(void) { return tot_num_mtrs; }
+ };
+
+class da_dc_brushed: public da_motor {
+private:
+	unsigned char enable_pin;
+	unsigned char pin0;
+	unsigned char pin1;
+public:
+	da_dc_brushed(orientation rot,
+		uchar tmax,
+		unsigned char en, 
+		unsigned char p0, 
+		unsigned char p1): da_motor(rot,tmax), enable_pin(en), 
+pin0(p0), pin1(p1) {
+	pinMode(enable_pin, OUTPUT);
+	pinMode(pin0, OUTPUT);
+	pinMode(pin1, OUTPUT);
+}
+	int setThrottle(uchar, travel);
+};
+  
+class da_lmu {
+private:
+  class da_motor *motors;
+  unsigned int operation_time_s;	/* time in operation in seconds */
+public:
+  da_lmu(class da_motor *motors): 
+  motors(motors) {}
+  class da_motor* motor(uchar mtr_num) { return &motors[mtr_num]; }
+};
